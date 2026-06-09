f932c829e570bc0e0c794a60b4f1070b67a403e8
Alan Labaš <alan.labas@student.um.si>
2026-06-09 15:43:29 +0200

    fix: link for linkedin

--- src/section/Contacts.tsx
+++ src/section/Contacts.tsx
@@ -7,7 +7,7 @@ export default function Contacts() {
         <section id="contact" className="min-h-screen flex flex-col items-center justify-center text-center px-6">
             <h1 className="text-5xl md:text-6xl font-bold text-gray-900 mb-4">{t.contact.title}</h1>
             <a href="https://github.com/Alan-Labas" target="_blank" rel="noreferrer" className="ml-2"><FaGithub className="text-4xl text-gray-500 hover:text-gray-700 transition-colors mb-4" /></a>
-            <a href="https://www.linkedin.com/in/alan-vunic-65323b201/" target="_blank" rel="noreferrer" className="ml-2"><FaLinkedin className="text-4xl text-gray-500 hover:text-gray-700 transition-colors mb-4" /></a>
+            <a href="https://www.linkedin.com/in/alan-labaš-65323b201" target="_blank" rel="noreferrer" className="ml-2"><FaLinkedin className="text-4xl text-gray-500 hover:text-gray-700 transition-colors mb-4" /></a>
             <a href="https://www.instagram.com/alanlabas/"><FaInstagram className="text-4xl text-gray-500 hover:text-gray-700 transition-colors mb-4" /></a>
         </section>
     )
